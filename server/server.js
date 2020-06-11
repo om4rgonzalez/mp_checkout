de5f036e1e42189057ec503aaac074b461a3949f
Marcelo Gonzalez <om4r.gonzalez@gmail.com>
2020-06-11 16:06:37 -0300

    Agregando https

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('../config/config');
 const express = require('express');
 const app = express();
+const https = require('https');
 
 
 //////////////////////////////////
@@ -19,7 +20,17 @@ app.use((req, res, next) => {
 //indice de rutas
 app.use(require('../routes'));
 
-app.listen(process.env.PORT, () => {
-    console.log('Usuario Escuchando el puerto ', process.env.PORT);
+// app.listen(process.env.PORT, () => {
+//     console.log('Usuario Escuchando el puerto ', process.env.PORT);
 
+// });
+
+let file_1 = '/etc/letsencrypt/live/bintelligence.net/privkey.pem';
+let file_2 = '/etc/letsencrypt/live/bintelligence.net/fullchain.pem';
+const httpsServer = https.createServer({
+    key: fs.readFileSync(file_1),
+    cert: fs.readFileSync(file_2),
+}, app);
+httpsServer.listen(process.env.PORT, () => {
+    console.log('HTTPS Server running');
 });
